e2946471febb5de74247caffa01507c46976ed73
Devin Altobello <dl.altobello@gmail.com>
2023-04-18 18:34:22 -0400

    Fix syntax errors for updated string variables and numbers

--- variables.js
+++ variables.js
@@ -1,14 +1,14 @@
-var email = "helloworld@gmail.com
+var email = "helloworld@gmail.com"
 var name = "Devin"
 var animal = "Penguin"
-var desiredSalary = 2,0000
-var bonus = 100,000
+var desiredSalary = 200000
+var bonus = 100000
 
 var rich = false
 var poor = true
 
 email = "goodbyeworld@gmail.com"
-firstName = Kevin
+firstName = "Kevin"
 animal = "Fish"
 
 console.log(email);
